45a315266432543206ac09f58b3c17b4fba483c3
Alex Efros <powerman@powerman.name>
2020-09-14 10:12:00 +0300

    ci: require lowercase PR subject

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -1,11 +1,3 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        // Start subject with upper case to have nicer ChangeLog in GitHub Releases.
-        'subject-case': [
-            2,
-            'always',
-            ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-        ],
-    },
 };
